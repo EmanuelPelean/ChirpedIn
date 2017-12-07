5055e65bf6cd0da640ad3aa5f02e74f88338efd5
Tim Johnson <pavementii@gmail.com>
2017-12-06 20:28:38 -0500

    updated signup.jsp, UserDto fields, created signup controller

--- WebContent/WEB-INF/views/signup.jsp
+++ WebContent/WEB-INF/views/signup.jsp
@@ -1,67 +1,131 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
-<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Sign Up</title>
 </head>
 <body>
-Sign Up Page
-<form:form action="signup" method="post" modelAttribute="command">
-<form:input type="text" name="firstname" path="firstname"></form:input>
-<button type="submit">Sign Up</button>
-</form:form>
+	
+	Sign Up Page
+	<form:form action="signup" method="post" modelAttribute="command">
+		<form:input type="text" name="firstname" path="linkedInFirstName"></form:input>
+		
 
-<!DOCTYPE html>
-<html>
-<body>
+	<p>For which skills would you like to HAVE a mentor?</p>
+	<h2>MENTEE SECTION</h2>
+		<h3>Front End</h3>
+		<form:checkbox path="mentorSkillsPhp" value="true"/> PHP <br>
+		<form:checkbox path="mentorSkillsJavaScript" value="true"/> JavaScript<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsHTML" value="true"/> HTML<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsCSS" value="true"/> CSS<br>
+		
 
-<p>For which skills would you like to HAVE a mentor?</p>
+		<h3>Back End</h3>
+		<form:checkbox name="mentorskills" path="mentorSkillsJava" value="true"/> Java<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsJsp" value="true"/> JSP<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsJstl" value="true"/> JSTL<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsSql" value="true"/> SQL<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsSpringMVC" value="true"/> Spring MVC<br>
+		<form:checkbox name="mentorskills" path="mentorSkillsHibernate" value="true"/> Hibernate<br>
+		
+	<!-- <input type="button" onclick="saveSkills(mentorskills, 'mentor')"
+			value="Save Mentee Data"> <br>
+		<br> <input type="text" id="mentorSkillsSummary" size="50">
+		<input type="submit" value="Submit">  -->	
 
-<form action="/signup">
-<input type="checkbox" name="mentorskills" value="java"> Java<br>
-<input type="checkbox" name="mentorskills" value="php"> PHP<br>
-<br>
-<input type="button" onclick="saveSkills(mentorskills)" value="Save Mentee Data">
-<br><br>
-<input type="text" id="order" size="50">
-<input type="submit" value="Submit">
-</form>
-  
-  
-  <p>For which skills would you like to BE a mentor?</p>
 
-<form action="/signup">
-<input type="checkbox" name="menteeskills" value="java"> Java<br>
-<input type="checkbox" name="menteeskills" value="php"> PHP<br>
-<br>
-<input type="button" onclick="saveSkills(mentorskills)" value="Save Mentoring Data">
-<br><br>
-<input type="text" id="order" size="50">
-<input type="submit" value="Submit">
-</form>
-  
-  
+	<p>For which skills would you like to BE a mentor?</p>
+	<h2>MENTOR SECTION</h2>
+		<h3>Front End</h3>
+		<form:checkbox name="menteeskills" path="menteeSkillsPhp" value="true"/> PHP<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsJavaScript" value="true"/> JavaScript<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsHTML" value="true"/> HTML<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsCSS" value="true"/> CSS<br>
+		
 
-<script>
-function saveSkills(object) {
-    var object = document.forms[0];
-    var txt = "";
-    var i;
-    for (i = 0; i < object.length; i++) {
-        if (object[i].checked) {
-            txt = txt + object[i].value + " ";
-        }
-    }
-    document.getElementById("order").value = "A mentor for: " + txt;
-}
-</script>
+		<h3>Back End</h3>
+		<form:checkbox name="menteeskills" path="menteeSkillsJava" value="true"/> Java<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsJsp" value="true"/> JSP<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsJstl" value="true"/> JSTL<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsSql" value="true"/> SQL<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsSpringMVC" value="true"/> Spring MVC<br>
+		<form:checkbox name="menteeskills" path="menteeSkillsHibernate" value="true"/> Hibernate<br>
 
-</body>
-</html>
+		<!-- <input type="button" onclick="saveSkills(menteeskills, 'mentee')"
+			value="Save Mentor Data"> <br>
+		<br> <input type="text" id="menteeSkillsSummary" size="50">
+		<input type="submit" value="Submit">  -->
 
+	<p>OR if you would like to just network?</p>
+	<h2>NETWORKING SECTION</h2>
+		<h3>Extracurricular</h3>
+		<form:checkbox name="networkskills" path="networkingFoodie"/> Foodie<br>
+		<form:checkbox name="networkskills" path="networkingGaming"/> Gamer<br>
+		<form:checkbox name="networkskills" path="networkingSports"/> Sports<br>
+		<form:checkbox name="networkskills" path="networkingAnime"/> Anime<br>
+		<form:checkbox name="networkskills" path="networkingFun"/> Fun<br> 
+		<br>
+		
+		<h3>Front End</h3>
+		<form:checkbox name="networkingskills" path="networkingSkillsPhp" value="true"/> PHP<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsJavaScript" value="true"/> JavaScript<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsHTML" value="true"/> HTML<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsCSS" value="true"/> CSS<br>
+		
+
+		<h3>Back End</h3>
+		<form:checkbox name="networkingskills" path="networkingSkillsJava" value="true"/> Java<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsJsp" value="true"/> JSP<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsJstl" value="true"/> JSTL<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsSql" value="true"/> SQL<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsSpringMVC" value="true"/> Spring MVC<br>
+		<form:checkbox name="networkingskills" path="networkingSkillsHibernate" value="true"/> Hibernate<br>
+
+		<input type="button" onclick="saveSkills(networkskills, 'network')"
+			value="Save Networker Data"> <br>
+		<br> <input type="text" id="networkSkillsSummary" size="50">
+		<input type="submit" value="Submit">
+		
+		<button type="submit">Update</button>
+	</form:form>
+
+	<script>
+		function saveSkills(object, context) {
+			var index;
+			console.log(document.forms);
+			if (context === "mentor") {
+				index = 1;
+			}else if (context === "mentee"){
+				index = 2;
+			}else {
+				index = 3;
+			}
+			var object = document.forms[index];
+			var txt = "";
+			var i;
+			for (i = 0; i < object.length; i++) {
+				if (object[i].checked) {
+					txt = txt + object[i].value + " ";
+					console.log(txt);
+				}
+			}
+
+			if (context === "mentor") {
+				document.getElementById("mentorSkillsSummary").value = "A mentor for: "
+						+ txt;
+			}else if (context === "mentee") {
+				document.getElementById("menteeSkillsSummary").value = "A mentee for: "
+					+ txt;
+			}else {
+				document.getElementById("networkSkillsSummary").value = "A networkee for: "
+					+ txt;
+			}
+		}
+	</script>
+	
 </body>
 </html>
--- src/com/gc/dto/UserDto.java
+++ src/com/gc/dto/UserDto.java
@@ -4,20 +4,366 @@
 package com.gc.dto;
 
 /**
- * @author Manu
+ * @author
  *
  */
 public class UserDto {
-private String firstname;
 
-public String getFirstname() {
-	return firstname;
-}
+	// LinkedIn Data
+	private String linkedInFirstName;
+	private String linkedInLastName;
+	private String linkedInHeadline;
+	private String linkedInLocation;
+	private String linkedInSummary;
+	private String linkedInPictureUrl;
+	private String linkedInLargePictureUrl;
+	private String linkedInPublicProfileUrl;
+	private String linkedInEmail;
+	private String linkedInId;
 
-public void setFirstname(String firstname) {
-	this.firstname = firstname;
-}
+	// ChirpedIn Data
 
+	// MENTOR BACKEND
+	private Boolean mentorSkillsJava;
+	private Boolean mentorSkillsJsp;
+	private Boolean mentorSkillsJstl;
+	private Boolean mentorSkillsSql;
+	private Boolean mentorSkillsJdbc;
+	private Boolean mentorSkillsSpringMVC;
+	private Boolean mentorSkillsHibernate;
 
+	// MENTOR FRONTEND
+	private Boolean mentorSkillsPhp;
+	private Boolean mentorSkillsJavaScript;
+	private Boolean mentorSkillsHTML;
+	private Boolean mentorSkillsCSS;
+
+	// MENTEE BACKEND
+	private Boolean menteeSkillsJava;
+	private Boolean menteeSkillsJsp;
+	private Boolean menteeSkillsJstl;
+	private Boolean menteeSkillsSql;
+	private Boolean menteeSkillsJdbc;
+	private Boolean menteeSkillsSpringMVC;
+	private Boolean menteeSkillsHibernate;
+
+	// MENTEE FRONTEND
+	private Boolean menteeSkillsPhp;
+	private Boolean menteeSkillsJavaScript;
+	private Boolean menteeSkillsHTML;
+	private Boolean menteeSkillsCSS;
+
+	// NETWORKING
+	private Boolean networkingFoodie;
+	private Boolean networkingGaming;
+	private Boolean networkingSports;
+	private Boolean networkingAnime;
+	private Boolean networkingFun;
+	
+	
+	private Boolean networkingSkillsJava;
+	private Boolean networkingSkillsJsp;
+	private Boolean networkingSkillsJstl;
+	private Boolean networkingSkillsSql;
+	private Boolean networkingSkillsJdbc;
+	private Boolean networkingSkillsSpringMVC;
+	private Boolean networkingSkillsHibernate;
+
+	// networking FRONTEND
+	private Boolean networkingSkillsPhp;
+	private Boolean networkingSkillsJavaScript;
+	private Boolean networkingSkillsHTML;
+	private Boolean networkingSkillsCSS;
+	
+	public String getLinkedInFirstName() {
+		return linkedInFirstName;
+	}
+	public void setLinkedInFirstName(String linkedInFirstName) {
+		this.linkedInFirstName = linkedInFirstName;
+	}
+	public String getLinkedInLastName() {
+		return linkedInLastName;
+	}
+	public void setLinkedInLastName(String linkedInLastName) {
+		this.linkedInLastName = linkedInLastName;
+	}
+	public String getLinkedInHeadline() {
+		return linkedInHeadline;
+	}
+	public void setLinkedInHeadline(String linkedInHeadline) {
+		this.linkedInHeadline = linkedInHeadline;
+	}
+	public String getLinkedInLocation() {
+		return linkedInLocation;
+	}
+	public void setLinkedInLocation(String linkedInLocation) {
+		this.linkedInLocation = linkedInLocation;
+	}
+	public String getLinkedInSummary() {
+		return linkedInSummary;
+	}
+	public void setLinkedInSummary(String linkedInSummary) {
+		this.linkedInSummary = linkedInSummary;
+	}
+	public String getLinkedInPictureUrl() {
+		return linkedInPictureUrl;
+	}
+	public void setLinkedInPictureUrl(String linkedInPictureUrl) {
+		this.linkedInPictureUrl = linkedInPictureUrl;
+	}
+	public String getLinkedInLargePictureUrl() {
+		return linkedInLargePictureUrl;
+	}
+	public void setLinkedInLargePictureUrl(String linkedInLargePictureUrl) {
+		this.linkedInLargePictureUrl = linkedInLargePictureUrl;
+	}
+	public String getLinkedInPublicProfileUrl() {
+		return linkedInPublicProfileUrl;
+	}
+	public void setLinkedInPublicProfileUrl(String linkedInPublicProfileUrl) {
+		this.linkedInPublicProfileUrl = linkedInPublicProfileUrl;
+	}
+	public String getLinkedInEmail() {
+		return linkedInEmail;
+	}
+	public void setLinkedInEmail(String linkedInEmail) {
+		this.linkedInEmail = linkedInEmail;
+	}
+	public String getLinkedInId() {
+		return linkedInId;
+	}
+	public void setLinkedInId(String linkedInId) {
+		this.linkedInId = linkedInId;
+	}
+	public Boolean getMentorSkillsJava() {
+		return mentorSkillsJava;
+	}
+	public void setMentorSkillsJava(Boolean mentorSkillsJava) {
+		this.mentorSkillsJava = mentorSkillsJava;
+	}
+	public Boolean getMentorSkillsJsp() {
+		return mentorSkillsJsp;
+	}
+	public void setMentorSkillsJsp(Boolean mentorSkillsJsp) {
+		this.mentorSkillsJsp = mentorSkillsJsp;
+	}
+	public Boolean getMentorSkillsJstl() {
+		return mentorSkillsJstl;
+	}
+	public void setMentorSkillsJstl(Boolean mentorSkillsJstl) {
+		this.mentorSkillsJstl = mentorSkillsJstl;
+	}
+	public Boolean getMentorSkillsSql() {
+		return mentorSkillsSql;
+	}
+	public void setMentorSkillsSql(Boolean mentorSkillsSql) {
+		this.mentorSkillsSql = mentorSkillsSql;
+	}
+	public Boolean getMentorSkillsJdbc() {
+		return mentorSkillsJdbc;
+	}
+	public void setMentorSkillsJdbc(Boolean mentorSkillsJdbc) {
+		this.mentorSkillsJdbc = mentorSkillsJdbc;
+	}
+	public Boolean getMentorSkillsSpringMVC() {
+		return mentorSkillsSpringMVC;
+	}
+	public void setMentorSkillsSpringMVC(Boolean mentorSkillsSpringMVC) {
+		this.mentorSkillsSpringMVC = mentorSkillsSpringMVC;
+	}
+	public Boolean getMentorSkillsHibernate() {
+		return mentorSkillsHibernate;
+	}
+	public void setMentorSkillsHibernate(Boolean mentorSkillsHibernate) {
+		this.mentorSkillsHibernate = mentorSkillsHibernate;
+	}
+	public Boolean getMentorSkillsPhp() {
+		return mentorSkillsPhp;
+	}
+	public void setMentorSkillsPhp(Boolean mentorSkillsPhp) {
+		this.mentorSkillsPhp = mentorSkillsPhp;
+	}
+	public Boolean getMentorSkillsJavaScript() {
+		return mentorSkillsJavaScript;
+	}
+	public void setMentorSkillsJavaScript(Boolean mentorSkillsJavaScript) {
+		this.mentorSkillsJavaScript = mentorSkillsJavaScript;
+	}
+	public Boolean getMentorSkillsHTML() {
+		return mentorSkillsHTML;
+	}
+	public void setMentorSkillsHTML(Boolean mentorSkillsHTML) {
+		this.mentorSkillsHTML = mentorSkillsHTML;
+	}
+	public Boolean getMentorSkillsCSS() {
+		return mentorSkillsCSS;
+	}
+	public void setMentorSkillsCSS(Boolean mentorSkillsCSS) {
+		this.mentorSkillsCSS = mentorSkillsCSS;
+	}
+	public Boolean getMenteeSkillsJava() {
+		return menteeSkillsJava;
+	}
+	public void setMenteeSkillsJava(Boolean menteeSkillsJava) {
+		this.menteeSkillsJava = menteeSkillsJava;
+	}
+	public Boolean getMenteeSkillsJsp() {
+		return menteeSkillsJsp;
+	}
+	public void setMenteeSkillsJsp(Boolean menteeSkillsJsp) {
+		this.menteeSkillsJsp = menteeSkillsJsp;
+	}
+	public Boolean getMenteeSkillsJstl() {
+		return menteeSkillsJstl;
+	}
+	public void setMenteeSkillsJstl(Boolean menteeSkillsJstl) {
+		this.menteeSkillsJstl = menteeSkillsJstl;
+	}
+	public Boolean getMenteeSkillsSql() {
+		return menteeSkillsSql;
+	}
+	public void setMenteeSkillsSql(Boolean menteeSkillsSql) {
+		this.menteeSkillsSql = menteeSkillsSql;
+	}
+	public Boolean getMenteeSkillsJdbc() {
+		return menteeSkillsJdbc;
+	}
+	public void setMenteeSkillsJdbc(Boolean menteeSkillsJdbc) {
+		this.menteeSkillsJdbc = menteeSkillsJdbc;
+	}
+	public Boolean getMenteeSkillsSpringMVC() {
+		return menteeSkillsSpringMVC;
+	}
+	public void setMenteeSkillsSpringMVC(Boolean menteeSkillsSpringMVC) {
+		this.menteeSkillsSpringMVC = menteeSkillsSpringMVC;
+	}
+	public Boolean getMenteeSkillsHibernate() {
+		return menteeSkillsHibernate;
+	}
+	public void setMenteeSkillsHibernate(Boolean menteeSkillsHibernate) {
+		this.menteeSkillsHibernate = menteeSkillsHibernate;
+	}
+	public Boolean getMenteeSkillsPhp() {
+		return menteeSkillsPhp;
+	}
+	public void setMenteeSkillsPhp(Boolean menteeSkillsPhp) {
+		this.menteeSkillsPhp = menteeSkillsPhp;
+	}
+	public Boolean getMenteeSkillsJavaScript() {
+		return menteeSkillsJavaScript;
+	}
+	public void setMenteeSkillsJavaScript(Boolean menteeSkillsJavaScript) {
+		this.menteeSkillsJavaScript = menteeSkillsJavaScript;
+	}
+	public Boolean getMenteeSkillsHTML() {
+		return menteeSkillsHTML;
+	}
+	public void setMenteeSkillsHTML(Boolean menteeSkillsHTML) {
+		this.menteeSkillsHTML = menteeSkillsHTML;
+	}
+	public Boolean getMenteeSkillsCSS() {
+		return menteeSkillsCSS;
+	}
+	public void setMenteeSkillsCSS(Boolean menteeSkillsCSS) {
+		this.menteeSkillsCSS = menteeSkillsCSS;
+	}
+	public Boolean getNetworkingFoodie() {
+		return networkingFoodie;
+	}
+	public void setNetworkingFoodie(Boolean networkingFoodie) {
+		this.networkingFoodie = networkingFoodie;
+	}
+	public Boolean getNetworkingGaming() {
+		return networkingGaming;
+	}
+	public void setNetworkingGaming(Boolean networkingGaming) {
+		this.networkingGaming = networkingGaming;
+	}
+	public Boolean getNetworkingSports() {
+		return networkingSports;
+	}
+	public void setNetworkingSports(Boolean networkingSports) {
+		this.networkingSports = networkingSports;
+	}
+	public Boolean getNetworkingAnime() {
+		return networkingAnime;
+	}
+	public void setNetworkingAnime(Boolean networkingAnime) {
+		this.networkingAnime = networkingAnime;
+	}
+	public Boolean getNetworkingFun() {
+		return networkingFun;
+	}
+	public void setNetworkingFun(Boolean networkingFun) {
+		this.networkingFun = networkingFun;
+	}
+	public Boolean getNetworkingSkillsJava() {
+		return networkingSkillsJava;
+	}
+	public void setNetworkingSkillsJava(Boolean networkingSkillsJava) {
+		this.networkingSkillsJava = networkingSkillsJava;
+	}
+	public Boolean getNetworkingSkillsJsp() {
+		return networkingSkillsJsp;
+	}
+	public void setNetworkingSkillsJsp(Boolean networkingSkillsJsp) {
+		this.networkingSkillsJsp = networkingSkillsJsp;
+	}
+	public Boolean getNetworkingSkillsJstl() {
+		return networkingSkillsJstl;
+	}
+	public void setNetworkingSkillsJstl(Boolean networkingSkillsJstl) {
+		this.networkingSkillsJstl = networkingSkillsJstl;
+	}
+	public Boolean getNetworkingSkillsSql() {
+		return networkingSkillsSql;
+	}
+	public void setNetworkingSkillsSql(Boolean networkingSkillsSql) {
+		this.networkingSkillsSql = networkingSkillsSql;
+	}
+	public Boolean getNetworkingSkillsJdbc() {
+		return networkingSkillsJdbc;
+	}
+	public void setNetworkingSkillsJdbc(Boolean networkingSkillsJdbc) {
+		this.networkingSkillsJdbc = networkingSkillsJdbc;
+	}
+	public Boolean getNetworkingSkillsSpringMVC() {
+		return networkingSkillsSpringMVC;
+	}
+	public void setNetworkingSkillsSpringMVC(Boolean networkingSkillsSpringMVC) {
+		this.networkingSkillsSpringMVC = networkingSkillsSpringMVC;
+	}
+	public Boolean getNetworkingSkillsHibernate() {
+		return networkingSkillsHibernate;
+	}
+	public void setNetworkingSkillsHibernate(Boolean networkingSkillsHibernate) {
+		this.networkingSkillsHibernate = networkingSkillsHibernate;
+	}
+	public Boolean getNetworkingSkillsPhp() {
+		return networkingSkillsPhp;
+	}
+	public void setNetworkingSkillsPhp(Boolean networkingSkillsPhp) {
+		this.networkingSkillsPhp = networkingSkillsPhp;
+	}
+	public Boolean getNetworkingSkillsJavaScript() {
+		return networkingSkillsJavaScript;
+	}
+	public void setNetworkingSkillsJavaScript(Boolean networkingSkillsJavaScript) {
+		this.networkingSkillsJavaScript = networkingSkillsJavaScript;
+	}
+	public Boolean getNetworkingSkillsHTML() {
+		return networkingSkillsHTML;
+	}
+	public void setNetworkingSkillsHTML(Boolean networkingSkillsHTML) {
+		this.networkingSkillsHTML = networkingSkillsHTML;
+	}
+	public Boolean getNetworkingSkillsCSS() {
+		return networkingSkillsCSS;
+	}
+	public void setNetworkingSkillsCSS(Boolean networkingSkillsCSS) {
+		this.networkingSkillsCSS = networkingSkillsCSS;
+	}
+
+	
 
 }
