02fe58064bbb7e572caab61cf4cac94af2e01361
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2017-12-13 09:16:29 -0500

    reformatted mapping order in HomeController for better readability

--- src/com/chirpedin/controllers/HomeController.java
+++ src/com/chirpedin/controllers/HomeController.java
@@ -43,36 +43,10 @@ import com.sun.xml.internal.ws.api.message.Header;
 @Controller
 public class HomeController {
 
-	// public static void main(String[] args) {
-	// ArrayList<String> skillsNeed = new ArrayList<String>();
-	//
-	// skillsNeed.add("php");
-	// skillsNeed.add("java");
-	// skillsNeed.add("css");
-	//
-	// ArrayList<String> skillsHave = new ArrayList<String>();
-	//
-	// skillsHave.add("php");
-	// // skillsHave.add("java");
-	// // skillsHave.add("css");
-	// int skillsCount = 0;
-	// for (String skill : skillsNeed) {
-	// if (skillsHave.contains(skill)) {
-	// skillsCount++;
-	// }
-	// }
-	//
-	// if (skillsCount == skillsNeed.size()) {
-	// System.out.println("#1");
-	// } else if (skillsCount < skillsNeed.size()) {
-	// System.out.println("#2");
-	// }
-	//
-	// }
-
+	
 	/**
 	 * Home page mapping (you can use "/" if you want) 1)Redirect users to request
-	 * LinkedIn access – (provide a value for the state and scopes you need) home
+	 * LinkedIn access (provide a value for the state and scopes you need) home
 	 * page has the hyperlink to do a get request to LinkedIN endpoit
 	 * 
 	 * @return
@@ -82,8 +56,109 @@ public class HomeController {
 
 		return new ModelAndView("home", "", "");// Since there is no model I could return string with view name
 	}
+	
+	/**
+	 * 3. Linked redirects back to this controller with a temporary code and
+	 * (returned state/once should match state passed in step 2) 4. Exchange
+	 * temporary code for an access token 5. Use access tokens to make calls to the
+	 * LinkedIn Api on behalf of the user
+	 * 
+	 * @param code
+	 *            - code from https://LinkedIn.com/login/oauth/authorize call
+	 * @param state
+	 *            - once value received from
+	 *            https://LinkedIn.com/login/oauth/authorize call to guard against
+	 *            cross site forgery attacks
+	 * @return
+	 */
+	@RequestMapping("/result")
+	public ModelAndView result(Model model, @RequestParam("code") String code, @RequestParam("state") String state) {
 
-	// this is called when the signup page is first opened
+		String jsonString = "";
+		String accessToken = "";
+		UserDto userDataDto = null;
+
+		try {
+			// 4. Exchange temporary code for an access token
+			URL url = new URL("https://www.linkedin.com/oauth/v2/accessToken?" + "grant_type=authorization_code&"
+					+ "code=" + code + "&" + "client_id=" + APICredentials.CLIENT_ID + "&"
+					+ "redirect_uri=http://localhost:8080/ChirpedIn/result&" + "client_secret="
+					+ APICredentials.CLIENT_SECRET);
+
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			con.setDoOutput(true); // Must add
+			con.setRequestMethod("POST");
+			con.setFixedLengthStreamingMode(0); // Must add
+			con.setRequestProperty("Accept", "application/json");
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			String currentLine = "";
+
+			while ((currentLine = reader.readLine()) != null) {
+				jsonString += currentLine;
+			}
+
+			JSONObject jsonObj = new JSONObject(jsonString);
+			accessToken = jsonObj.getString("access_token");
+			con.disconnect();
+
+			// 5. Use access token to make calls to the LinkedIn Api on behalf of the user
+
+			url = new URL(
+					"https://api.linkedin.com/v1/people/~:(id,email-address,first-name,last-name,headline,location,summary,picture-url,picture-urls::(original),public-profile-url)?format=json&oauth2_access_token="
+							+ accessToken);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+
+			reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			currentLine = "";
+			jsonString = "";
+
+			while ((currentLine = reader.readLine()) != null) {
+				jsonString += currentLine;
+			}
+
+			// Get the user data we want from the json string
+			System.out.println(jsonString);
+			jsonObj = new JSONObject(jsonString);
+			JSONObject jsonLocation = (JSONObject) jsonObj.get("location");
+			JSONObject jsonLargePic = (JSONObject) jsonObj.get("pictureUrls");
+
+			userDataDto = new UserDto();
+
+			userDataDto.setLinkedInId((String) jsonObj.get("id"));
+			userDataDto.setLinkedInFirstName((String) jsonObj.get("firstName"));
+			userDataDto.setLinkedInLastName((String) jsonObj.get("lastName"));
+			userDataDto.setLinkedInHeadline((String) jsonObj.get("headline"));
+			userDataDto.setLinkedInLocation((String) jsonLocation.get("name"));
+			userDataDto.setLinkedInPictureUrl((String) jsonObj.get("pictureUrl"));
+			userDataDto.setLinkedInLargePictureUrl(jsonLargePic.optString("values"));
+			userDataDto.setLinkedInPublicProfileUrl((String) jsonObj.get("publicProfileUrl"));
+			userDataDto.setLinkedInEmail((String) jsonObj.get("emailAddress"));
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		/*
+		 * TODO Change to return the name and avatar image in the instead of the raw
+		 * json data the model
+		 */
+		model.addAttribute("data", userDataDto);
+		/*we then go to the "signup.jsp" page and pass in a new UserDto
+		 * this UserDto is attached to the form on the signup.jsp page by "command"
+		 */
+		return new ModelAndView("signup", "command", new UserDto());
+	}
+
+	// this is called when the signup page is opened without going through the "results" mapping
 	@RequestMapping({ "/signup" })
 	public ModelAndView signupPage(Model model) {
 		System.out.println("Regular Signup");
@@ -171,116 +246,7 @@ public class HomeController {
 
 	}
 
-	/**
-	 * 3. Linked redirects back to this controller with a temporary code – and
-	 * (returned state/once should match state passed in step 2) 4. Exchange
-	 * temporary code for an access token 5. Use access tokens to make calls to the
-	 * LinkedIn Api on behalf of the user
-	 * 
-	 * @param code
-	 *            - code from https://LinkedIn.com/login/oauth/authorize call
-	 * @param state
-	 *            - once value received from
-	 *            https://LinkedIn.com/login/oauth/authorize call to guard against
-	 *            cross site forgery attacks
-	 * @return
-	 */
-	@RequestMapping("/result")
-	public ModelAndView result(Model model, @RequestParam("code") String code, @RequestParam("state") String state) {
-
-		String jsonString = "";
-		String accessToken = "";
-		UserDto userDataDto = null;
-
-		try {
-			// 4. Exchange temporary code for an access token
-			URL url = new URL("https://www.linkedin.com/oauth/v2/accessToken?" + "grant_type=authorization_code&"
-					+ "code=" + code + "&" + "client_id=" + APICredentials.CLIENT_ID + "&"
-					+ "redirect_uri=http://localhost:8080/ChirpedIn/result&" + "client_secret="
-					+ APICredentials.CLIENT_SECRET);
-
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-			con.setDoOutput(true); // Must add
-			con.setRequestMethod("POST");
-			con.setFixedLengthStreamingMode(0); // Must add
-			con.setRequestProperty("Accept", "application/json");
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-			String currentLine = "";
-
-			while ((currentLine = reader.readLine()) != null) {
-				jsonString += currentLine;
-			}
-
-			JSONObject jsonObj = new JSONObject(jsonString);
-			accessToken = jsonObj.getString("access_token");
-			con.disconnect();
-
-			// 5. Use access token to make calls to the LinkedIn Api on behalf of the user
-
-			url = new URL(
-					"https://api.linkedin.com/v1/people/~:(id,email-address,first-name,last-name,headline,location,summary,picture-url,picture-urls::(original),public-profile-url)?format=json&oauth2_access_token="
-							+ accessToken);
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-
-			reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-			currentLine = "";
-			jsonString = "";
-
-			while ((currentLine = reader.readLine()) != null) {
-				jsonString += currentLine;
-			}
-
-			// Get the user data we want from the json string
-			System.out.println(jsonString);
-			jsonObj = new JSONObject(jsonString);
-			JSONObject jsonLocation = (JSONObject) jsonObj.get("location");
-			JSONObject jsonLargePic = (JSONObject) jsonObj.get("pictureUrls");
-
-			// String userID = (String) jsonObj.get("id");
-			// String firstName = (String) jsonObj.get("firstName");
-			// String lastName = (String) jsonObj.get("lastName");
-			// String headline = (String) jsonObj.get("headline");
-			// String location = (String) jsonLocation.get("name");
-			// String smallPic = (String) jsonObj.get("pictureUrl");
-			// JSONObject jsonLargePicValues = (JSONObject) jsonLargePic.get("values");
-			// String largePic = (String) jsonLocation.get("values");
-			// String largePic = (String) jsonObj.get("values");
-			// String profileUrl = (String) jsonObj.get("publicProfileUrl");
-			// String email = (String) jsonObj.get("email-address");
-
-			userDataDto = new UserDto();
-
-			userDataDto.setLinkedInId((String) jsonObj.get("id"));
-			userDataDto.setLinkedInFirstName((String) jsonObj.get("firstName"));
-			userDataDto.setLinkedInLastName((String) jsonObj.get("lastName"));
-			userDataDto.setLinkedInHeadline((String) jsonObj.get("headline"));
-			userDataDto.setLinkedInLocation((String) jsonLocation.get("name"));
-			userDataDto.setLinkedInPictureUrl((String) jsonObj.get("pictureUrl"));
-			userDataDto.setLinkedInLargePictureUrl(jsonLargePic.optString("values"));
-			userDataDto.setLinkedInPublicProfileUrl((String) jsonObj.get("publicProfileUrl"));
-			userDataDto.setLinkedInEmail((String) jsonObj.get("emailAddress"));
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		/*
-		 * TODO Change to return the name and avatar image in the instead of the raw
-		 * json data the model
-		 */
-		model.addAttribute("data", userDataDto);
-		return new ModelAndView("signup", "command", new UserDto());
-	}
-
+	
 	@RequestMapping("/matches")
 	public String showMatches(Model model) {
 
